978573d2e8c26f0fd298ddfea7efe355f4161590
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-01-25 03:22:12 -0800

    Added Conditional Logic

--- Pizza_Ordering_Chatbot.py
+++ Pizza_Ordering_Chatbot.py
@@ -2,8 +2,10 @@ print("Hello, my name is Alex your virtual assistent. I will help you order a pi
 print("I need to ask you a few questions, after typing your answer, press enter.")
 print("\n")
 userName = input("\nEnter  your name:  ")
-print("\n")
-print("\nHello, " + userName + " . Nice to meet you!")
+if userName.lower() == "david prato":
+    print("\nMy creator, " + userName +  ". Pleasure to serve you!")
+else:
+    print("\nHello, " + userName + " . Nice to meet you!")
 size = input("\nWhat size do you want?  Enter small, medium, or large:  ")
 flavor = input("\nEnter the flavor of your pizza:  ")
 crustType = input("\nWhat kind of crust do you want:  ")
@@ -11,9 +13,22 @@ quantity = input("\nHow many of these do you want to order, use only numeric val
 quantity = int(quantity)
 method = input("\nIs this carry out or delivery:  ")
 salesTax = 1.1
-pizzaCost = 14.99
-total = (pizzaCost * quantity) * salesTax
+if size.lower() == "small":
+    pizzaCost = 8.99
+elif size.lower() == "medium":
+    pizzaCost = 14.99
+elif size.lower() == "large":
+     pizzaCost = 17.99
+if method.lower() == "delivery":
+    deliveryFee = 5
+else:
+    deliveryFee = 0
+total = (pizzaCost * quantity) * salesTax + deliveryFee
 print("\n--------------------------")
 print("Thank you", userName, "for your order.")
 print("Your", quantity, size, flavor, "pizza with", crustType, "crust costs", "${:,.2f}".format(total) + "." )
+if total >= 50:
+    print("\nCongradulations, you've been awarded a $10 Off couponfor your next order!")
+else:
+    print("\nOrders over $50 will receive a free $10 Off coupon!")
 print("----------------------------")
